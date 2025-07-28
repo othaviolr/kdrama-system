14c0b929e3ef8785ae16226b236f2594e3135d62
othav <othav@OTHVIO>
2025-07-28 20:58:48 -0300

    tests: add delete profile methods

---
+++ tests/KDramaSystem.Tests/Application/UseCases/Usuario/DeletarPerfilUseCaseTests.cs
@@ -0,0 +1,49 @@
+﻿using KDramaSystem.Application.Interfaces;
+using KDramaSystem.Application.UseCases.Usuario.Deletar;
+using KDramaSystem.Domain.Entities;
+using KDramaSystem.Domain.Interfaces.Repositories;
+using Moq;
+
+public class DeletarPerfilUseCaseTests
+{
+    private readonly Mock<IUsuarioRepository> _usuarioRepoMock;
+    private readonly Mock<IUsuarioAutenticadoProvider> _usuarioAuthProviderMock;
+    private readonly DeletarPerfilUseCase _useCase;
+
+    public DeletarPerfilUseCaseTests()
+    {
+        _usuarioRepoMock = new Mock<IUsuarioRepository>();
+        _usuarioAuthProviderMock = new Mock<IUsuarioAutenticadoProvider>();
+
+        _useCase = new DeletarPerfilUseCase(_usuarioRepoMock.Object, _usuarioAuthProviderMock.Object);
+    }
+
+    [Fact]
+    public async Task ExecutarAsync_UsuarioEncontrado_DeveRemoverUsuario()
+    {
+        var usuarioId = Guid.NewGuid();
+        var usuario = new Usuario(usuarioId, "Usuário", "usuariokdrama", "email@exemplo.com");
+
+        _usuarioAuthProviderMock.Setup(x => x.ObterUsuarioId()).Returns(usuarioId);
+        _usuarioRepoMock.Setup(x => x.ObterPorIdAsync(usuarioId)).ReturnsAsync(usuario);
+        _usuarioRepoMock.Setup(x => x.RemoverAsync(usuario.Id)).Returns(Task.CompletedTask);
+
+        await _useCase.ExecutarAsync();
+
+        _usuarioRepoMock.Verify(x => x.RemoverAsync(usuario.Id), Times.Once);
+    }
+
+    [Fact]
+    public async Task ExecutarAsync_UsuarioNaoEncontrado_DeveLancarExcecao()
+    {
+        var usuarioId = Guid.NewGuid();
+
+        _usuarioAuthProviderMock.Setup(x => x.ObterUsuarioId()).Returns(usuarioId);
+        _usuarioRepoMock.Setup(x => x.ObterPorIdAsync(usuarioId)).ReturnsAsync((Usuario?)null);
+
+        var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _useCase.ExecutarAsync());
+        Assert.Equal("Usuário não encontrado.", ex.Message);
+
+        _usuarioRepoMock.Verify(x => x.RemoverAsync(It.IsAny<Guid>()), Times.Never);
+    }
+}
