7909e763f5e5989bdb899dc2ea84f9c8c283d534
othav <othav@OTHVIO>
2025-07-30 18:12:07 -0300

    chore(Dorama): create useCases of dorama

---
+++ src/KDramaSystem.Infrastructure/Repositories/DoramaRepository.cs
@@ -0,0 +1,27 @@
+﻿using KDramaSystem.Domain.Entities;
+using KDramaSystem.Domain.Interfaces;
+
+namespace KDramaSystem.Infrastructure.Repositories;
+
+public class DoramaRepository : IDoramaRepository
+{
+    private readonly List<Dorama> _doramas = new();
+
+    public Task AdicionarAsync(Dorama dorama)
+    {
+        _doramas.Add(dorama);
+        return Task.CompletedTask;
+    }
+
+    public Task<Dorama?> ObterPorIdAsync(Guid id)
+    {
+        var dorama = _doramas.FirstOrDefault(d => d.Id == id);
+        return Task.FromResult(dorama);
+    }
+
+    public Task<bool> ExisteComTituloAsync(string titulo)
+    {
+        var exists = _doramas.Any(d => d.Titulo == titulo);
+        return Task.FromResult(exists);
+    }
+}
---
+++ src/KDramaSystem.Infrastructure/Repositories/GeneroRepository.cs
@@ -0,0 +1,27 @@
+﻿using KDramaSystem.Domain.Entities;
+using KDramaSystem.Domain.Interfaces;
+
+namespace KDramaSystem.Infrastructure.Repositories;
+
+public class GeneroRepository : IGeneroRepository
+{
+    private readonly List<Genero> _generos = new();
+
+    public Task<List<Genero>> ObterPorIdsAsync(List<Guid> ids)
+    {
+        var generos = _generos.Where(g => ids.Contains(g.Id)).ToList();
+        return Task.FromResult(generos);
+    }
+
+    public Task<Genero?> ObterPorIdAsync(Guid id)
+    {
+        var genero = _generos.FirstOrDefault(g => g.Id == id);
+        return Task.FromResult(genero);
+    }
+
+    public Task AdicionarAsync(Genero genero)
+    {
+        _generos.Add(genero);
+        return Task.CompletedTask;
+    }
+}
