0f13213d94f7e2fcfc655750529011f6827c6ef0
othav <othav@OTHVIO>
2025-08-08 19:05:44 -0300

    feat(User): create public profile mode

---
+++ src/KDramaSystem.Application/UseCases/Usuario/ObterPerfilPublico/IObterPerfilPublicoUseCase.cs
@@ -0,0 +1,8 @@
+﻿using KDramaSystem.Application.UseCases.Usuario.Dtos;
+
+namespace KDramaSystem.Application.UseCases.Usuario.ObterPerfilPublico;
+
+public interface IObterPerfilPublicoUseCase
+{
+    Task<PerfilPublicoDto?> ExecutarAsync(string nomeUsuario, Guid? usuarioLogadoId = null);
+}
