002a1539c64877da8e625f919697a4269e012108
othav <othav@OTHVIO>
2025-08-12 16:43:23 -0300

    feat(Avaliacao): create repository implementation

---
+++ src/KDramaSystem.Domain/Interfaces/IAvaliacaoRepository.cs
@@ -0,0 +1,11 @@
+﻿using KDramaSystem.Domain.Entities;
+
+namespace KDramaSystem.Domain.Interfaces;
+
+public interface IAvaliacaoRepository
+{
+    Task<Avaliacao?> ObterPorUsuarioETemporadaAsync(Guid usuarioId, Guid temporadaId);
+    Task<bool> ExisteAvaliacaoAsync(Guid usuarioId, Guid temporadaId);
+    Task AdicionarAsync(Avaliacao avaliacao);
+    Task AtualizarAsync(Avaliacao avaliacao);
+}
