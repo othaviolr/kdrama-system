fd8a11a0a7f3776baecf14a372fe36f8ace87dee
othav <othav@OTHVIO>
2025-08-13 20:41:47 -0300

    feat(UseCases): create dtos represeating functions

---
+++ src/KDramaSystem.Application/DTOs/ListaPrateleira/CriarListaPrateleiraDto.cs
@@ -0,0 +1,10 @@
+﻿namespace KDramaSystem.Application.DTOs.ListaPrateleira;
+
+public class CriarListaPrateleiraDto
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = null!;
+    public string? Descricao { get; set; }
+    public string? ImagemCapaUrl { get; set; }
+    public string? ShareToken { get; set; }
+}
---
+++ src/KDramaSystem.Application/DTOs/ListaPrateleira/EditarListaPrateleiraDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KDramaSystem.Application.DTOs.ListaPrateleira;
+
+public class EditarListaPrateleiraDto
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = null!;
+    public string? Descricao { get; set; }
+    public string? ImagemCapaUrl { get; set; }
+}
