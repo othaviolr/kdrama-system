b7cb3ed2962dbf91b55893737d01d2c7f8b7859a
othav <othav@OTHVIO>
2025-08-13 21:14:30 -0300

    refactor(Controller): adjusting create method

---
+++ src/KDramaSystem.Application/DTOs/ListaPrateleira/CriarListaPrateleiraRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using KDramaSystem.Domain.Enums;
+
+namespace KDramaSystem.Application.DTOs.ListaPrateleira;
+
+public class CriarListaPrateleiraRequestDto
+{
+    public string Nome { get; set; } = null!;
+    public string? Descricao { get; set; }
+    public string? ImagemCapaUrl { get; set; }
+    public ListaPrivacidade Privacidade { get; set; }
+}
