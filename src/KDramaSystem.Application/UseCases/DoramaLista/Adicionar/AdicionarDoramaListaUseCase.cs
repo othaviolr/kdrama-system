d48489d04e9d22b91423955986308dbc67bd4934
othav <othav@OTHVIO>
2025-08-14 17:53:12 -0300

    feat(UseCases): create add and remove dramas of list

---
+++ src/KDramaSystem.Application/UseCases/DoramaLista/Adicionar/AdicionarDoramaListaUseCase.cs
@@ -0,0 +1,19 @@
+﻿using KDramaSystem.Domain.Interfaces.Repositories;
+
+namespace KDramaSystem.Application.UseCases.DoramaLista.Adicionar;
+
+public class AdicionarDoramaListaUseCase
+{
+    private readonly IDoramaListaRepository _repository;
+
+    public AdicionarDoramaListaUseCase(IDoramaListaRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task ExecuteAsync(Guid listaId, Guid doramaId)
+    {
+        var item = new Domain.Entities.DoramaLista(Guid.NewGuid(), listaId, doramaId);
+        await _repository.AdicionarAsync(item);
+    }
+}
