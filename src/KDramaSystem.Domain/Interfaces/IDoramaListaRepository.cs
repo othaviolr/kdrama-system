a1563fe4efc6a81815ec3838a8e67a61c6f1f3e7
othav <othav@OTHVIO>
2025-08-14 17:43:47 -0300

    feat(Infrastructure): create repository of kdrama-list

---
+++ src/KDramaSystem.Domain/Interfaces/IDoramaListaRepository.cs
@@ -0,0 +1,10 @@
+﻿using KDramaSystem.Domain.Entities;
+
+namespace KDramaSystem.Domain.Interfaces.Repositories;
+
+public interface IDoramaListaRepository
+{
+    Task AdicionarAsync(DoramaLista doramaLista);
+    Task RemoverAsync(Guid listaPrateleiraId, Guid doramaId);
+    Task<List<DoramaLista>> ObterPorListaIdAsync(Guid listaPrateleiraId);
+}
