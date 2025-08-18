468074fa4a9eaf6c691c79dabb813a2c8039a45c
othav <othav@OTHVIO>
2025-08-18 19:25:35 -0300

    feat(dto): create dto's for atividade registration and getter

---
+++ src/KDramaSystem.Application/DTOs/Atividade/ObterAtividadeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KDramaSystem.Application.DTOs.Atividade;
+
+public class ObterAtividadeDto
+{
+    public Guid Id { get; set; }
+    public string Tipo { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public DateTime Data { get; set; }
+}
---
+++ src/KDramaSystem.Application/DTOs/Atividade/RegistrarAtividadeDto.cs
@@ -0,0 +1,10 @@
+﻿using KDramaSystem.Domain.ValueObjects;
+
+namespace KDramaSystem.Application.DTOs.Atividade;
+
+public class RegistrarAtividadeDto
+{
+    public Guid UsuarioId { get; set; }
+    public TipoAtividade Tipo { get; set; } = null!;
+    public Guid ReferenciaId { get; set; }
+}
