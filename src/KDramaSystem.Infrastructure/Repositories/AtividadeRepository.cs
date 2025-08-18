d89fc239d88e9a38b5da54f56a03245aa6445f18
othav <othav@OTHVIO>
2025-08-18 19:02:01 -0300

    feat(Infrastructure): create Atividade interface and implementation

---
+++ src/KDramaSystem.Infrastructure/Repositories/AtividadeRepository.cs
@@ -0,0 +1,38 @@
+﻿using KDramaSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using KDramaSystem.Infrastructure.Persistence;
+
+namespace KDramaSystem.Infrastructure.Repositories;
+
+public class AtividadeRepository : IAtividadeRepository
+{
+    private readonly KDramaDbContext _context;
+
+    public AtividadeRepository(KDramaDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task RegistrarAsync(Atividade atividade)
+    {
+        await _context.Atividades.AddAsync(atividade);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Atividade>> ObterPorUsuarioAsync(Guid usuarioId)
+    {
+        return await _context.Atividades
+            .Where(a => a.UsuarioId == usuarioId)
+            .OrderByDescending(a => a.Data)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Atividade>> ObterFeedAsync(Guid usuarioId, IEnumerable<Guid> seguindoIds)
+    {
+        var usuariosIds = seguindoIds.Append(usuarioId);
+        return await _context.Atividades
+            .Where(a => usuariosIds.Contains(a.UsuarioId))
+            .OrderByDescending(a => a.Data)
+            .ToListAsync();
+    }
+}
