fca051f84e5b3be1bf8f089652f29d728ca57944
othav <othav@OTHVIO>
2025-08-20 17:26:33 -0300

    chore(DependencyInjection): add activity services di

---
+++ src/KDramaSystem.API/Controllers/AtividadeController.cs
@@ -0,0 +1,28 @@
+﻿using KDramaSystem.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/atividades")]
+public class AtividadeController : ControllerBase
+{
+    private readonly IAtividadeService _atividadeService;
+
+    public AtividadeController(IAtividadeService atividadeService)
+    {
+        _atividadeService = atividadeService;
+    }
+
+    [HttpGet("feed/{quantidade}")]
+    public async Task<IActionResult> ObterFeed(int quantidade)
+    {
+        var atividades = await _atividadeService.ObterFeedAsync(quantidade);
+        return Ok(atividades);
+    }
+
+    [HttpGet("usuario/{usuarioId}")]
+    public async Task<IActionResult> ObterAtividadesUsuario(Guid usuarioId)
+    {
+        var atividades = await _atividadeService.ObterAtividadesUsuarioAsync(usuarioId);
+        return Ok(atividades);
+    }
+}
