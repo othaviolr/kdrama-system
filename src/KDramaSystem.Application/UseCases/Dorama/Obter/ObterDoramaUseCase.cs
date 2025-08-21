cd6718a05fc54e6be7e467e86bdbf0dc3e2fdc48
othav <othav@OTHVIO>
2025-08-21 17:43:41 -0300

    hotfix(UseCase): fix get dorama for id

--- src/KDramaSystem.Application/UseCases/Dorama/Obter/ObterDoramaUseCase.cs
+++ src/KDramaSystem.Application/UseCases/Dorama/Obter/ObterDoramaUseCase.cs
@@ -1,4 +1,4 @@
-﻿using KDramaSystem.Domain.Entities;
+﻿using KDramaSystem.Application.DTOs.Dorama;
 using KDramaSystem.Domain.Interfaces;
 
 namespace KDramaSystem.Application.UseCases.Dorama.Obter;
@@ -6,17 +6,41 @@ namespace KDramaSystem.Application.UseCases.Dorama.Obter;
 public class ObterDoramaUseCase
 {
     private readonly IDoramaRepository _doramaRepository;
+
     public ObterDoramaUseCase(IDoramaRepository doramaRepository)
     {
         _doramaRepository = doramaRepository;
     }
-    public async Task<KDramaSystem.Domain.Entities.Dorama> ExecutarAsync(ObterDoramaRequest request)
+
+    public async Task<ObterDoramaDto?> ExecutarAsync(ObterDoramaRequest request)
     {
         var dorama = await _doramaRepository.ObterPorIdAsync(request.Id);
         if (dorama == null)
+            return null;
+
+        return new ObterDoramaDto
         {
-            throw new Exception("Dorama não encontrado.");
-        }
-        return dorama;
+            DoramaId = dorama.Id,
+            Titulo = dorama.Titulo,
+            TituloOriginal = dorama.TituloOriginal,
+            Sinopse = dorama.Sinopse,
+            CapaUrl = dorama.ImagemCapaUrl,
+            AnoLancamento = dorama.AnoLancamento,
+            PaisOrigem = dorama.PaisOrigem,
+            EmExibicao = dorama.EmExibicao,
+            Plataforma = dorama.Plataforma,
+            Generos = dorama.Generos.Select(g => new ObterDoramaDto.GeneroDto
+            {
+                Id = g.Id,
+                Nome = g.Nome
+            }).ToList(),
+            Atores = dorama.Atores
+                .Where(da => da.Ator != null)
+                .Select(da => new ObterDoramaDto.AtorDto
+                {
+                    Id = da.Ator.Id,
+                    Nome = da.Ator.Nome
+                }).ToList()
+        };
     }
 }
