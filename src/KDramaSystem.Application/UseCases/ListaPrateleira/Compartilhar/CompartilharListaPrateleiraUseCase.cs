6230188f7fcccb7ab3cabb39e7382f05d08dc5e3
othaviolr <othavionogueira2003@gmail.com>
2025-08-26 00:29:46 -0300

    feat(UseCase): created a function to generate token to share your own list

---
+++ src/KDramaSystem.Application/UseCases/ListaPrateleira/Compartilhar/CompartilharListaPrateleiraUseCase.cs
@@ -0,0 +1,39 @@
+﻿using KDramaSystem.Application.UseCases.ListaPrateleira.Obter;
+using KDramaSystem.Domain.Interfaces.Repositories;
+
+namespace KDramaSystem.Application.UseCases.ListaPrateleira.Compartilhar;
+
+public class CompartilharListaPrateleiraRequest
+{
+    public Guid ListaId { get; set; }
+    public Guid UsuarioId { get; set; }
+}
+
+public class CompartilharListaPrateleiraUseCase
+{
+    private readonly IListaPrateleiraRepository _repository;
+    private readonly ObterListaPrateleiraUseCase _obterUseCase;
+
+    public CompartilharListaPrateleiraUseCase(IListaPrateleiraRepository repository, ObterListaPrateleiraUseCase obterUseCase)
+    {
+        _repository = repository;
+        _obterUseCase = obterUseCase;
+    }
+
+    public async Task<string> ExecuteAsync(CompartilharListaPrateleiraRequest request, CancellationToken cancellationToken = default)
+    {
+        var lista = (await _obterUseCase.ExecuteAsync(new ObterListaPrateleiraRequest
+        {
+            ListaId = request.ListaId,
+            UsuarioLogadoId = request.UsuarioId
+        })).FirstOrDefault();
+
+        if (lista == null)
+            throw new Exception("Lista não encontrada.");
+
+        lista.GerarShareToken();
+        await _repository.AtualizarAsync(lista, cancellationToken);
+
+        return lista.ShareToken!;
+    }
+}
