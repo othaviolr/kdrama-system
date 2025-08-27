f1bb343bf27e13c96b4fdd4c7687b9dc8fc6a306
othaviolr <othavionogueira2003@gmail.com>
2025-08-27 00:03:33 -0300

    chore(Repository): new method to repository to query all following and followed

--- src/KDramaSystem.Application/Interfaces/Repositories/IUsuarioRelacionamentoRepository.cs
+++ src/KDramaSystem.Application/Interfaces/Repositories/IUsuarioRelacionamentoRepository.cs
@@ -7,4 +7,6 @@ public interface IUsuarioRelacionamentoRepository
     Task<bool> ExisteRelacionamentoAsync(Guid seguidorId, Guid seguindoId);
     Task CriarAsync(UsuarioRelacionamento relacionamento);
     Task RemoverAsync(Guid seguidorId, Guid seguindoId);
+    Task<List<Usuario>> ObterSeguidoresAsync(Guid usuarioId);
+    Task<List<Usuario>> ObterSeguindoAsync(Guid usuarioId);
 }
