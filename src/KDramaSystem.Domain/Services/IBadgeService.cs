d90f42d6d64293064fdc0a72a96a633f8b4915f8
othaviolr <othavionogueira2003@gmail.com>
2025-09-11 19:44:32 -0300

    hotfix(Service): fix method of designing badge to user

---
+++ src/KDramaSystem.Domain/Services/IBadgeService.cs
@@ -0,0 +1,8 @@
+﻿using KDramaSystem.Domain.Entities;
+
+namespace KDramaSystem.Domain.Services;
+
+public interface IBadgeService
+{
+    Task<List<Badge>> ConcederBadgesProgressaoAsync(Guid usuarioId);
+}
